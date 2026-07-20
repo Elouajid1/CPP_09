da8136c3c5a2e04b0d5c1702a935774f772d98ae
Mohamed Amine El Ouajid <mel-ouaj@c3r2p4.1337.ma>
2026-07-20 17:13:39 +0100

    second

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -2,10 +2,20 @@
 #define PMERGEME_HPP
 
 #include <iostream>
+#include <deque>
+#include <vector>
 
 class PmergeMe
 {
-    
+	private :
+		std::deque<int> first;
+		std::vector<int> second;
+	
+	public :
+		PmergeMe();
+		PmergeMe(const PmergeMe& cpy);
+		PmergeMe& operator=(const PmergeMe& other);
+		~PmergeMe();
 };
 
 #endif
