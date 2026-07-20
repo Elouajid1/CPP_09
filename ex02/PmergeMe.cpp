384c61a088e487cfb9b43cd35f3ed688908f4f2c
Mohamed Amine El Ouajid <mel-ouaj@c3r2p4.1337.ma>
2026-07-20 20:00:29 +0100

    plus

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,2 +1,21 @@
 #include "PmergeMe.hpp"
 
+PmergeMe::PmergeMe()
+{}
+
+PmergeMe::PmergeMe(const PmergeMe& cpy) : first(cpy.first), second(cpy.second)
+{}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other)
+{
+    if (this != &other)
+    {
+        first = other.first;
+        second = other.second;
+    }
+    return (*this);
+}
+
+PmergeMe::~PmergeMe()
+{}
+
